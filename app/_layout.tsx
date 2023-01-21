fe3293597fec0512caff73fec449d36ab7e82c5e
matheusmichels <matheusmichels@outlook.com.br>
2023-01-21 16:52:17 -0300

    Setup main layout

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Slot } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function Layout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slot />
+    </GestureHandlerRootView>
+  );
+}
